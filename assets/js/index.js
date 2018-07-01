6a807f2cab8bdf30f3bf389b52bb3575589b9be9
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2018-07-02 01:04:12 +0530

    discussion form made and styled

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,18 @@
 var formDisplay = document.querySelector("#hidden");
 var replyButton = document.querySelector("#reply-button");
+var formDisplay1 = document.querySelector("#hidden1");
+var replyButton1 = document.querySelector("#reply-button1");
+var formDisplay2 = document.querySelector("#hidden2");
+var replyButton2 = document.querySelector("#reply-button2");
 
 replyButton.addEventListener("click",function(){
-    formDisplay.style.visibility = "visible";
+    formDisplay.style.visibility="visible";
+});
+
+replyButton1.addEventListener("click",function(){
+    formDisplay1.style.visibility="visible";
+});
+
+replyButton2.addEventListener("click",function(){
+    formDisplay2.style.visibility="visible";
 });
