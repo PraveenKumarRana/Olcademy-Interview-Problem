68e7102f97e5e632b80418e437396943ea0c9899
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2018-07-02 10:09:24 +0530

    Final version for submission- Check before submiting

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,15 +4,38 @@ var formDisplay1 = document.querySelector("#hidden1");
 var replyButton1 = document.querySelector("#reply-button1");
 var formDisplay2 = document.querySelector("#hidden2");
 var replyButton2 = document.querySelector("#reply-button2");
+var hideForm     = document.getElementsByTagName("body");
+var showForm     = document.getElementById("show-more");
+
+
+//==============================================
+//              REPLY BUTTON
+//==============================================
+// For checking purpose this will be and in final production loop will be used for the same perpose
 
 replyButton.addEventListener("click",function(){
     formDisplay.style.visibility="visible";
+    formDisplay1.style.visibility="hidden";
+    formDisplay2.style.visibility="hidden";
 });
 
 replyButton1.addEventListener("click",function(){
     formDisplay1.style.visibility="visible";
+    formDisplay.style.visibility="hidden";
+    formDisplay2.style.visibility="hidden";
 });
 
 replyButton2.addEventListener("click",function(){
+    formDisplay.style.visibility="hidden";
+    formDisplay1.style.visibility="hidden";
     formDisplay2.style.visibility="visible";
+});
+
+
+//==============================================
+//            SHOW MORE LOGIC
+//==============================================
+
+showForm.addEventListener("click",function(){
+    alert("This will load other posts from the database after connecting the database in backend");
 });
---
+++ readme.md
@@ -0,0 +1,12 @@
+* This only contain Frontend Part of the Webpage.
+* Show more button will load the posts once the database is connected.
+        - This will seperate the posts for different section such as Electronics, Kitchen ets on the basis of ID which will be stored in the database and can be loaded to the display Page.
+        - Also Search / Ast New will also subimitting there contents to the database and they will have there own ID will will be helping in displaying the posts
+        - All post will be displayed on the Discussion Forum using loop.
+* Reply button is connected to the reply form which is hidden. When the user will click the reply button then
+  he/ she will see the form.
+
+* I have tested this in Chrome and Safari , which provides me the best user Experiance.
+
+* THANKS FOR READING
+* Hope to work with your Company.
